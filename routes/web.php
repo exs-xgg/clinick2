1529fd8ad7e11879f64889c7c35a088764880045
XGG <zapperhatz26@gmail.com>
2021-07-18 20:51:02 +0800

    added classes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[ DashboardController::class, 'index']);
