d518a4c96563f5e107dcb975a8267b318769ae45
XGG <zapperhatz26@gmail.com>
2021-07-18 20:31:55 +0800

    adjusted patient data set

--- database/migrations/2021_07_18_120908_create_patients_table.php
+++ database/migrations/2021_07_18_120908_create_patients_table.php
@@ -24,6 +24,7 @@ class CreatePatientsTable extends Migration
             $table->string('contact_no', 15)->nullable();
             $table->string('civil_stat', 100)->nullable();
             $table->string('occupation', 100)->nullable();
+            $table->string('hmo', 100)->nullable();
 
             $table->timestamps();
         });
