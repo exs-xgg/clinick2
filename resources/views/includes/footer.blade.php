6748331e9608b5784726aa71aac4e59a546f706f
XGG <zapperhatz26@gmail.com>
2021-07-18 20:06:47 +0800

    add form blocking script

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -4,4 +4,13 @@
         </div>
   </body>
   <script src="js/app.js" type="text/javascript"></script>
+  <script>
+    $('form').on('keyup keypress', function(e) {
+      var keyCode = e.keyCode || e.which;
+      if (keyCode === 13) {
+          e.preventDefault();
+          return false;
+        }
+    });
+  </script>
 </html>
