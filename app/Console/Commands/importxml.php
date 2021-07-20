2e95275e9518e7b5a9e6c766355c880764c1d716
XGG <zapperhatz26@gmail.com>
2021-07-20 11:59:27 +0800

    finalize array structure

--- app/Console/Commands/importxml.php
+++ app/Console/Commands/importxml.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Exception;
-
+use Carbon\Carbon;
 class importxml extends Command
 {
     /**
@@ -44,43 +44,107 @@ class importxml extends Command
 
 
         $infoxml=simplexml_load_file("InfoDB.xml") or die("Error: Cannot create object");
-        $rs = $infoxml->xpath('//InfoDB/Username[.="ABAD, ANTHONY"]');
-        foreach ($rs as $key) {
-            $records[] = (($rs[0]??null)->xpath("parent::*"))[0]->ID->__toString();
-        }
         $count = 0;
+        $out = [];
         foreach ($dataxml as $key) {
+
+            echo $count . ' = '. $key->ID->__toString() . PHP_EOL;
+
             try {
                 $myfile = fopen("Data Library/" . $key->ID->__toString() . "/user-resx.CLINICK", "r") or die("Unable to open file!");
-                $records =  explode("\r\n",fread($myfile,filesize("Data Library/1/user-resx.CLINICK")));
-            } catch (Exception $e) {
-                $records = $infoxml->xpath('/InfoDB/Username[.="TIMBANG, EMMANUEL"]');
-            }finally{
-                echo $count . ' = '. $key->ID->__toString() . PHP_EOL;
+                $fstream = fread($myfile,filesize("Data Library/" . $key->ID->__toString() . "/user-resx.CLINICK"));
+                $records =  explode("\r\n",$fstream);
 
-                array_walk($records, array($this, 'transform_date'));
-                $x[] = [
+                $reps = [];
+                foreach ($records as $a) {
+                    if ($a){
+                        $a = trim($a);
+                        $a = $this->transform_date($a);
+                        $info = $infoxml->xpath('//InfoDB/ID[.="'.$a.'"]');
+                        if($info){
+                            $parent = (($info[0]??null)->xpath("parent::*"))[0];
+
+                            $reps[] = ['created_at' =>($a),
+                             'updated_at' =>($a),
+                             'symptoms' => $parent->Proc->__toString()??null,
+                             'diagnosis' => $parent->Diagnosis->__toString()??null,
+                             'prescription' => $parent->Meds->__toString()??null,
+                             'history' => $parent->Notes->__toString()??null];
+                        }else{
+                            echo "xxxxxxxxxxxxxxxxxxxxxx $a" . PHP_EOL;
+                            $out[] = $a;
+                        }
+                    }
+                }
+
+            } catch (Exception $e) {
+                $reps = [];
+                $recs = $infoxml->xpath('//InfoDB/Username[.="'.$key->Name1->__toString().'"]');
+                foreach ($recs as $k) {
+                    $parent = (($k[0]??null)->xpath("parent::*"))[0];
+                    $reps[] = [
+                        'created_at' => $this->toDate($parent->ID->__toString()),
+                        'updated_at' => $this->toDate($parent->ID->__toString()),
+                        'symptoms' => $parent->Proc->__toString(),
+                        'diagnosis' => $parent->Diagnosis->__toString(),
+                        'prescription' => $parent->Meds->__toString(),
+                        'history' => $parent->Notes->__toString()
+                    ];
+                }
+
+
+            }finally{
+
+                $f = [
                     'id' => $key->ID->__toString(),
                     'lname' => $key->Name1->__toString(),
                     'birthdate' => $key->Age->__toString(),
-                    'sex' => $key->Sex->__toString(),
-                    'civil_stat' => $key->CivilStat->__toString(),
-                    'contact_no' => $key->Contact->__toString(),
+                    'sex' => $this->isDash($key->Sex->__toString()),
+                    'civil_stat' =>  $this->isDash($key->CivilStat->__toString(),1),
+                    'contact_no' =>  $this->isDash($key->Contact->__toString()),
                     'occupation' => $key->Occuption->__toString(),
                     'hmo' => $key->HMO->__toString(),
                     'address' => $key->Address->__toString(),
-                    'visit' => $records
+                    'visit' => $reps
                 ];
-                $count++;
+                $x[] = $f;
             }
+            $count++;
 
         }
-        dd($x[6350]);
+        dd($out);
     }
 
-    public function transform_date(&$item1, $key){
+    public function transform_date($item1){
         $item1 = str_replace('/','+',$item1);
         $item1 = str_replace(':','',$item1);
         $item1 = str_replace(' ','',$item1);
+        return $item1;
+    }
+    public function toDate($date){
+        $date = explode('+',$date);
+        try {
+            $year = substr($date[2],0,4);
+            $timeH = explode('',substr($date[2],4,2));
+            $timeM = explode('',substr($date[2],6,2));
+            $timeA = explode('',substr($date[2],10,2));
+            return Carbon::parse($year . '-' . $date[0] . '-' . $date[1] . ' ' . $timeH[0] . ':' . $timeM[0] . ' ' . $timeA[0])->toString();
+        } catch (Exception $e) {
+            return Carbon::parse('')->toString();
+        }
+
+    }
+
+    public function isDash($str,$cap=0){
+        if ($str == '-----') {
+            return 'NA';
+        }else{
+            if($cap==1){
+                return strtoupper($str[0] ?? '');
+            }else{
+
+                return ($str[0] ?? '');
+            }
+        }
     }
 }
