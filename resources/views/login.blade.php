7d83bd7e983c1aebdfbad9d9c7125489ac723bb7
XGG <zapperhatz26@gmail.com>
2021-09-08 11:13:00 +0800

    add login blade

---
+++ resources/views/login.blade.php
@@ -0,0 +1,26 @@
+@extends('includes.template')
+@section('content')
+
+<div class="container">
+
+    <div class="col-lg-6 col-md-8 col-sm-12 mx-auto">
+        <div class="card">
+            <div class="card-header">
+                Login
+            </div>
+            <div class="card-body">
+                <form class="form-signin" action="/login" method="post">
+                    @csrf
+                    <label for="inputEmail" class="sr-only">Username</label>
+                    <input type="email" id="inputEmail" class="form-control mb-3" placeholder="Username" required autofocus>
+                    <label for="inputPassword" class="sr-only">Password</label>
+                    <input type="password" id="inputPassword" class="form-control mb-3" placeholder="Password" required>
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                  </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@stop
