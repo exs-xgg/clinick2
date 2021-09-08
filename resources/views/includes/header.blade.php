41068436aaf9cf6ee94e4c40b440e7be7c545022
XGG <zapperhatz26@gmail.com>
2021-09-08 11:14:39 +0800

    code polishing

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -13,22 +13,23 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
+            @auth
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                      <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item active">
-                      <a class="nav-link" href="/create-patient">Create Patient</a>
-                    </li>
-                </ul>
-              <form class="form-inline my-2 my-lg-0" action="/search" method="get">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search" required minlength="3">
-                <button class="btn btn-info my-2 my-sm-0" type="submit">Search</button>
-              </form>
-
-              <button class="btn btn-danger my-2 my-sm-0 ml-3">Logout</button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                        <a class="nav-link" href="/create-patient">Create Patient</a>
+                        </li>
+                    </ul>
+                <form class="form-inline my-2 my-lg-0" action="/search" method="get">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search" required minlength="3">
+                    <button class="btn btn-info my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                <button class="btn btn-danger my-2 my-sm-0 ml-3">Logout</button>
+            @endauth
             </div>
           </nav>
           <div class="container-fluid">
