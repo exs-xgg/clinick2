2952c93bad894795274092af4e244cd321bdeb5f
exs-xgg <zapperhatz26@gmail.com>
2023-01-21 21:06:38 +0800

    add new MCR stuff

---
+++ app/Http/Controllers/VitalsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vitals;
+use Illuminate\Http\Request;
+
+class VitalsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Vitals  $vitals
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vitals $vitals)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Vitals  $vitals
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vitals $vitals)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Vitals  $vitals
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Vitals $vitals)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Vitals  $vitals
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vitals $vitals)
+    {
+        //
+    }
+}
